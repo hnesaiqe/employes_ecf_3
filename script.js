4214e443a37a53c86084229a3fd720f7f4d7b7e9
hnesaiqe <s.hne@cci-formation.nc>
2021-04-28 22:51:34 +1100

    request PUT,POST and GET

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
 let url = "https://6057e432c3f49200173ad08d.mockapi.io/api/v1/employees/";
 var x = document.getElementById("tdList");
 
-function displayContent(data) {
-
-    for (let i = 0; i < data.length; i++) {
-        console.log(data[i])
-
-        x.innerHTML +=
-            '<tr id="row ' + data[i].id + ' ">' +
-            '<th id="td">' + data[i].name + '</th>' +
-            '<td>' + data[i].last_name + '</td>' +
-            '<td><button type="button" id="btnDelete" class="btn btn-danger" onclick = btnDel(' + data[i].id +',this) >Delete</button></td > ' +
-            '<td><button type="button" id=" viewMore' + data[i].id + '" onclick = btnView(' + data[i].id + ') name="view" class="viewMore btn btn-secondary">View More</button></td >' +
-            '</tr>';
-    };
-}
-
-// Afficher un Post
-
+// Afficher /n Post
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
@@ -27,19 +11,62 @@ xhttp.onreadystatechange = function () {
 xhttp.open("GET", url, true);
 xhttp.send();
 
+function displayContent(data) {
+
+    for (let i = 0; i < data.length; i++) {
+        console.log(data[i])
+
+        x.innerHTML +=
+            '<tr id="row ' + data[i].id + ' ">' +
+            '<th id="td">' + data[i].name + '</th>' +
+            '<td>' + data[i].last_name + '</td>' +
+            '<td><button type="button" id="btnDelete" class="btn btn-danger" onclick = btnDel(' + data[i].id + ',this) >Delete</button></td > ' +
+            '<td><button type="button" id=" viewMore' + data[i].id + '" onclick = btnView(' + data[i].id + ') name="view" class="viewMore btn btn-secondary" data-toggle="modal" data-target="#exampleModal">View More</button></td >' +
+            '</tr>';
+    };
+}
+
+// Fonction /1 Modal post
 
 function btnView(ID) {
 
     console.log(ID);
+    document.getElementById("title").innerText = 'id = ' + ID;
 
-    alert(ID)
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            content(JSON.parse(this.responseText));
+        }
+    };
+    xhttp.open("GET", url + ID, true);
+    xhttp.send();
+
+
+
+
 }
 
+// Afficher /1 post
 
+
+function content(data) {
+    console.log(data.name)
+    var az = document.getElementById("modalFooter");
+    document.getElementById("modalName").value =  data.name;
+    
+   document.getElementById("modalLastName").value = data.last_name;
+     document.getElementById("modalJobTitle").value = data.job_title;
+   document.getElementById("modalEmail").value = data.email;
+    az.innerHTML += 
+        '<button type="button" class="btn btn-primary" onclick="btnEdit(' + data.id + ')">Save changes</button>';
+
+
+};
 
 // Request Delete
 
-function btnDel(ID,e) {
+function btnDel(ID, e) {
 
     var ask = confirm("Êtes-vous sûr de vouloir supprimer le Post?");
 
@@ -55,4 +82,38 @@ function btnDel(ID,e) {
         xhttp.open("DELETE", url + ID, true);
         xhttp.send();
     }
+}
+// Request Post
+
+function loadDoc() {
+
+    var name = document.getElementById("newName").value;
+    var lastName = document.getElementById("newLastName").value;
+    var jobTitle = document.getElementById("newJobTitle").value;
+    var email = document.getElementById("newEmail").value;
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.open("POST", url, true);
+    xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhttp.send("name=" + name + "&last_name=" + lastName + "&job_title=" + jobTitle + "&email=" + email);
+
+    document.getElementById('myForm').reset();
+}
+
+// Request Put
+
+function btnEdit(ID) {
+
+    var name = document.getElementById("modalName").value;
+    var lastName = document.getElementById("modalLastName").value;
+    var jobTitle = document.getElementById("modalJobTitle").value;
+    var email = document.getElementById("modalEmail").value;
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.open("PUT", url + ID, true);
+    xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhttp.send("name=" + name + "&last_name=" + lastName + "&job_title=" + jobTitle + "&email=" + email);
+
 }
